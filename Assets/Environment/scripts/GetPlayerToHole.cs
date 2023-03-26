c5ba4c01ff7ee7fd8ec8b3c3a2ae31e2d0bb80d0
lunzard <1433732143@qq.com>
2023-03-26 21:55:24 +0800

    feat(envir): toggle grappling lines when teleporting

--- Assets/Environment/scripts/GetPlayerToHole.cs
+++ Assets/Environment/scripts/GetPlayerToHole.cs
@@ -16,6 +16,7 @@ public class GetPlayerToHole : MonoBehaviour
     private GameObject player;
     private Rigidbody rb;
     private Vector3 targetPosition;
+    private GrappleController grappleController;
     // Start is called before the first frame update
     //void Start()
     //{
@@ -27,26 +28,7 @@ public class GetPlayerToHole : MonoBehaviour
     {
         if (isDiving)
         {
-            if (player.transform.position == target1.transform.position)
-            {
-                targetPosition = target2.transform.position;
-                speed = jumpSpeed;
-            }
-            else if (player.transform.position == target2.transform.position)
-            {
-                targetPosition = target3.transform.position;
-                speed = fallingSpeed;
-            }
-            else if (player.transform.position == target3.transform.position)
-            {
-                isDiving = false;
-                rb.useGravity = true;
-                speed = runningSpeed;
-            }
-            else
-            {
-                player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, speed * Time.deltaTime);
-            }
+            teleport_to_underwater();
         }
     }
     private void OnTriggerEnter(Collider collision)
@@ -61,8 +43,37 @@ public class GetPlayerToHole : MonoBehaviour
                 targetPosition = target1.transform.position;
                 rb = player.GetComponent<Rigidbody>();
                 rb.useGravity = false;
+                grappleController = player.GetComponent<GrappleController>();
+                grappleController.SetGrappleActive(Grappleable.Hand.Left, false);
+                grappleController.SetGrappleActive(Grappleable.Hand.Right, false);
 
             }
+        }
+    }
+
+    void teleport_to_underwater()
+    {
+        if (player.transform.position == target1.transform.position)
+        {
+            targetPosition = target2.transform.position;
+            speed = jumpSpeed;
+        }
+        else if (player.transform.position == target2.transform.position)
+        {
+            targetPosition = target3.transform.position;
+            speed = fallingSpeed;
+        }
+        else if (player.transform.position == target3.transform.position)
+        {
+            isDiving = false;
+            rb.useGravity = true;
+            speed = runningSpeed;
+            grappleController.SetGrappleActive(Grappleable.Hand.Left, true);
+            grappleController.SetGrappleActive(Grappleable.Hand.Right, true);
+        }
+        else
+        {
+            player.transform.position = Vector3.MoveTowards(player.transform.position, targetPosition, speed * Time.deltaTime);
         }
     }
 }

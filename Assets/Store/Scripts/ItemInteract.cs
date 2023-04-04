22320807eeb1540c8646deb07ff040e9f4c1db67
ian-from-dover <ian09hong@gmail.com>
2023-04-04 23:54:59 +0800

    fix(item): guard clause for no shop instance

--- Assets/Store/Scripts/ItemInteract.cs
+++ Assets/Store/Scripts/ItemInteract.cs
@@ -8,6 +8,7 @@ public class ItemInteract : XRGrabInteractable
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
+        if (ShopManager.Instance == null) return;
         if (item != null) ShopManager.Instance.ShowPanels(item);
     }
 }

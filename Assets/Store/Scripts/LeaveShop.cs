b4b6bcfd4a24a785dbcba4e6c7a085b6b444f83c
ian-from-dover <ian09hong@gmail.com>
2023-04-09 19:05:54 +0800

    fix(tut): fix light and door bugs in tut-shop

--- Assets/Store/Scripts/LeaveShop.cs
+++ Assets/Store/Scripts/LeaveShop.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LeaveShop : MonoBehaviour
 {
@@ -7,7 +8,8 @@ public class LeaveShop : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         // TODO: change the hand tag on the player prefab to player too.
-        if (other.gameObject.CompareTag("Player"))
+        // if (other.gameObject.CompareTag("Player"))
+        if (other.GetComponent<XRDirectInteractor>() != null)
         {
             leave.Invoke();
         }
--- Assets/TutorialStage/Scripts/SceneTransitAdditive.cs
+++ Assets/TutorialStage/Scripts/SceneTransitAdditive.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,20 +10,29 @@ public class SceneTransitAdditive : MonoBehaviour
 {
     [Header("ChangeScene() is called by some event.")]
     [SerializeField] private string sceneName;
-    private bool _isLoaded = false;
+    private bool _wasLoaded = false;
     
     public void AddScene()
     {
-        if (!_isLoaded) SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-        _isLoaded = true;
+        if (_wasLoaded) return;
+        StartCoroutine(AddSceneRoutine());
     }
+
+    IEnumerator AddSceneRoutine()
+    {
+        AsyncOperation loadSceneProcess = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        yield return new WaitUntil(() => loadSceneProcess.isDone);
+        _wasLoaded = true;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    }
+    
     /// <summary>
     /// Can be used by Unity Event trigger functions
     /// </summary>
     /// <param name="nextSceneName"></param>
     public void AddSceneTo(string nextSceneName)
     {
-        if (!_isLoaded) SceneManager.LoadSceneAsync(nextSceneName, LoadSceneMode.Additive);
-        _isLoaded = true;
+        if (!_wasLoaded) SceneManager.LoadSceneAsync(nextSceneName, LoadSceneMode.Additive);
+        _wasLoaded = true;
     }
 }
